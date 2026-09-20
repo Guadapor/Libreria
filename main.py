876bfe9dd2af917bded51305419769bf25ff861d
Guadalupe Portillo <portilloguadalupe558@gmail.com>
2026-09-20 18:54:21 -0300

    commit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from database.db import iniciar_base_datos
+from modelos.usuarios import crear_admin_inicial
+from vistas.menu_principal import abrir_login
+from vistas.menu_princiapal import abrir_menu
 
 
 def main(): 
     iniciar_base_datos() 
+    crear_admin_inicial()
+
+    usuario = abrir_login() 
+    if usuario:
+        abrir_menu(usuario)
+        
 
 if __name__ == "__main__":
     main()
---
+++ modelos/usuarios.py
@@ -0,0 +1,40 @@
+import hashlib
+from database.db import obtener_conexion
+
+def encriptar (contrasena):
+    return hashlib.sha256(contrasena.encode("utf-8")).hexdigest()
+
+def crear_admin_inicial():
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+    cursor.execute("SELEC COUNT(*) FROM usuarios")
+    cantidad = cursor.fetchone()[0]
+    if cantidad == 0:
+        cursor.execute( 
+            "INSERT INTO usuarios (nombre, nombre_usuario, contrasena, rol) VALUES (?, ?, ?, ?)",
+            ("Administrador", "admin", encriptar("admin123"), "administrador"),    
+        )
+        conexion.commit()
+    conexion.close()
+
+def validar_login(nombre_usuario, contrasena):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT id_usuario, nombre, nombre_usuario, rol"
+                   "FROM usuarios WHERE nombre_usuario = ? AND contrasena = ?",
+                   (nombre_usuario, encriptar(contrasena)),
+                   )
+    fila = cursor.fetchone()
+    conexion.close()
+
+    if fila is None:
+        return None
+    return {
+        "id_usuario": fila[0],
+        "nombre": fila[1],
+        "nombre_usuario": fila[2],
+        "rol": fila[3],
+    }
+
+    
+    
