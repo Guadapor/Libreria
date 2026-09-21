942f764ffcbcd8eae7b8de447818b40636881fa4
Guadalupe Portillo <portilloguadalupe558@gmail.com>
2026-09-21 18:42:41 -0300

    commit

--- modelos/ventas.py
+++ modelos/ventas.py
@@ -5,16 +5,20 @@ def registrar_venta(id_usuario, productos):
     conexion = obtener_conexion()
     cursor = conexion.cursor()
 
-     try:
+    try:
         total = sum(
-            producto['precio_unitario'] * producto['cantidad'] 
+            producto['precio_unitario'] * producto['cantidad']
             for producto in productos
- )
+        )
+        fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-     fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
-     cursor.execute(
-
-      "INSERT INTO ventas (fecha, total, id_usuario) VALUES (?, ?, ?)",
-      (fecha, total, id_usuario )
-)
+        cursor.execute(
+            "INSERT INTO ventas (fecha, total, id_usuario) VALUES (?, ?, ?)",
+            (fecha, total, id_usuario)
+        )
+        conexion.commit()
+    except Exception:
+        conexion.rollback()
+        raise
+    finally:
+        conexion.close()
