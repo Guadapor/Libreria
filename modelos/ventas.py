5a21e40a38d8d1570caa409332882288793f70f1
JuliRivera <jriveracs06@gmail.com>
2026-09-20 22:58:52 -0300

    Agregar registro de ventas

---
+++ modelos/ventas.py
@@ -0,0 +1,20 @@
+from database.db import obtener_conexion
+from datetime import datetime
+
+def registrar_venta(id_usuario, productos):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+     try:
+        total = sum(
+            producto['precio_unitario'] * producto['cantidad'] 
+            for producto in productos
+ )
+
+     fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+     cursor.execute(
+
+      "INSERT INTO ventas (fecha, total, id_usuario) VALUES (?, ?, ?)",
+      (fecha, total, id_usuario )
+)
